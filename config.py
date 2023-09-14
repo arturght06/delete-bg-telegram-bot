a4127e38405dcc38509ebdd1fa0f84c5a8f99190
arturght06 <artslep321@gmail.com>
2023-09-14 15:21:43 +0200

    First commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # token = '5420434712:AAE8GQX8bUDwNcD4gzKL3ftJwmwJEcQpLyQ' # reserve
-token = '5546594390:AAEpEMSF69_5JLapZ-A6rBNq9u7tEF2bR7I' # main
-adminPanelPasswords = ["/adminmMxXK7cPUH1pRLIPhDeuYO6EdzkwyX1JMyYpSPvr"]
-admin_id = [904245039]
-mongodb_cluster_link = 'mongodb+srv://Admin:root@cluster0.wa7hj.mongodb.net/userdata?retryWrites=true&w=majority'
+token = '' # telegram bot token
+adminPanelPasswords = ["/admin123"] # password for admin panel
+admin_id = [12345678]
+mongodb_cluster_link = '' # mongodb cluster link
